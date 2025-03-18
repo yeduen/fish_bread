8537fea9f12064c9721b5d58f184cc87f7b0e71c
yeduen <gaurga@naver.com>
2025-03-19 02:10:00 +0900

    변경사항 저장

--- fishbread.py
+++ fishbread.py
@@ -1,3 +1,23 @@
+sales ={
+    "팥붕어빵":0,
+    "슈크림붕어빵":0,
+    "초코붕어빵":0
+}
+
+
+#붕어빵 가격이 필요
+price ={
+    "팥붕어빵":1000,
+    "슈크림붕어빵":1200,
+    "초코붕어빵":1500
+}
+
+def calculate_sales():
+    total_sales = 0
+    for key in sales: #sales의 key값을 꺼내온다
+        total_sales += (price[key] * sales[key])
+    print(f"오늘의 총 매출은 {total_sales}입니다.")
+
 while True:
     mode = input("원하는 모드를 선택하세요 (주문, 관리자, 종료) :")
     if mode == "종료":
@@ -8,6 +28,6 @@ while True:
     elif mede == "관리자":
         admin_mode()
 
+calculate_sales()      
 
-    
-        
+           
